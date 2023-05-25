e5c156cb4fbb4823cb3d6cd900fb359bb4a4c9d1
Moskic <39458557+Moskic@users.noreply.github.com>
2023-05-25 19:21:09 +1200

    Update converter.bat
    Fixed the issue of not being able to handle multiple files.

--- converter.bat
+++ converter.bat
@@ -1,5 +1,7 @@
 @echo off
 
+set ffmpegPath=%~dp0ffmpeg
+
 :loop
 if "%~1"=="" goto :end
 
@@ -12,13 +14,13 @@ set extension=%~x1
 rem Check if the extension is .ts, .mkv or .flv
 if /I "%extension%"==".ts" (
     rem Convert .ts file to .mp4 file
-    %~dp0ffmpeg -i "%input%" -c:v copy -c:a copy "%output%"
+    "%ffmpegPath%" -i "%input%" -c:v copy -c:a copy "%output%"
 ) else if /I "%extension%"==".mkv" (
     rem Convert .mkv file to .mp4 file
-    %~dp0ffmpeg -i "%input%" -c:v copy -c:a copy "%output%"
+    "%ffmpegPath%" -i "%input%" -c:v copy -c:a copy "%output%"
 ) else if /I "%extension%"==".flv" (
     rem Convert .flv file to .mp4 file
-    %~dp0ffmpeg -i "%input%" -c:v copy -c:a copy "%output%"
+    "%ffmpegPath%" -i "%input%" -c:v copy -c:a copy "%output%"
 ) else (
     echo Unsupported file type: %extension%
 )
